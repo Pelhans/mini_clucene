479fd792914cc66a642c2c9ac7e69c5642bde3a9
Pelhans <me@pelhans.com>
2023-01-14 06:47:27 +0800

    add

---
+++ demo/Main.cpp
@@ -0,0 +1,11 @@
+
+#include "CLucene/util/Misc.h"
+
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+
+int main( int32_t argc, char** argv ){
+    uint64_t str = Misc::currentTimeMillis();
+
+}
